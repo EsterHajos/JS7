dd08d7a7a0cde0c24b6b24246d3fa210bec71b04
EsterHajos <ester.marta.hajos@gmail.com>
2026-05-08 10:49:24 +0200

    added my own code (function and event)

--- Scripts-JS/index.js
+++ Scripts-JS/index.js
@@ -1,3 +1,64 @@
+//Add a random word to DOM 
+funtion addWordToDom() {
+  let randomIndex = Math.floor(Math.random() * words.length);
+  randomWord = words[randomIndex];
+  word.innerHTML = randomWord;
+}
+
+console.log(randomWord);
+
+// Score 
+function updateScore() {
+  score++;
+
+  scoreEl.innerHTML = score;
+}
+
+console.log(score); 
+
+// Time (do I have to have this?)
+function updateTime() {
+ time = time - 1;
+
+  timeEl.innerText = time;
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+} 
+
+console.log(time);  
+
+// Game over
+function gameOver() {
+  endgameEl.style.display = "flex";
+  endgameEl.innerHTML = `
+    <h1>Game Over!</h1>,
+    <p>Score: ${score}</p>,
+  `;
+}
+
+console.log(endgameEl);
+
+addWordToDom();
+
+let Timer = setInterval(updateTime, 1000);
+
+
+
+text.addEventListener("input", function (e) {
+  if (text.value === randomWord) {
+    addWordToDom();
+    updateScore();
+    text.value = "";
+    time = time + 3; //varför la jag 3 här? 
+    timeEl.innerText = time;
+  }
+});
+
+
+
 // Variables for the DOM elements
 const word = document.getElementById("word");
 const text = document.getElementById("text");
